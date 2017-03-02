0ca7cce8e464fa14a1baacf7a35cbc7bd740ecc3
JimmyGit <JimmyGit@users.noreply.github.com>
2017-03-02 15:04:58 -0800

    Update compareAndSort.js

--- compareAndSort.js
+++ compareAndSort.js
@@ -1,23 +1,22 @@
 /***********************
 Compare two arrays and combine the non-duplicate values
 
-Array = [10, 20, 30]
-Array2 = [1, 10, 15, 30]
+var Array = ["10","20","30"];
+var Array2 = ["1","3", "5", "10"];
 
-Combined Array = [1, 10, 15,20, 30]
+Combined Array = ["1", "3", "5", "10", "20", "30"]
 
 ***********************/
 
-var arr = [10, 20, 30];
-var arr1 = [1, 10, 15, 30];
+var arr = ["10","20","30"];
+var arr1 = ["1","3", "5", "10"];
 
 for(var i = 0; i < arr1.length; i++) {
   
   if (arr.indexOf(arr1[i]) < 0){
-    var len = arr.length;
-    arr[arr.length] = arr1[i];
+    arr.push(arr1[i]);
   }
 
 }
 
-console.log(arr.sort());
+console.log(arr.sort(function(a,b){return a- b}));
